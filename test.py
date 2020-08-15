1a4883a7b4fd676f5108f24c61dbab3443a39e84
shasagar <shashaa35@gmail.com>
2020-08-15 10:55:34 +0530

    adding first assignment

---
+++ test.py
@@ -0,0 +1,36 @@
+import operator
+
+
+class Student:
+
+    def __init__(self, name, age, marks, rollNumber):
+        self.name = name
+        self.age = age
+        self.marks = marks
+        self.rollNumber = rollNumber
+
+    def __str__(self):
+        return "name: {0}, age: {1}, marks:{2}, rollNumber:{3}".format(self.name, self.age, self.marks, self.rollNumber)
+
+
+def printlist(listOfObjects):
+    for obj in listOfObjects:
+        print(str(obj))
+
+
+def sort(students, criteria):
+    keys = criteria.split(',')
+    filteredKeys = []
+    for key in keys:
+        filteredKeys.append(key.strip())
+    # todo: add validation for the keys if they actually exists in the Class
+    return sorted(students, key=operator.attrgetter(*filteredKeys))
+
+
+studentList = [Student("shashank", 25, 95, "13cse053"), Student("shashank2", 25, 92, "13cse052"),
+               Student("shivam", 27, 89, "13cse045"), Student("vijay", 23, 98, "14cse050")]
+
+printlist(studentList)
+studentList = sort(studentList, "age, marks")
+print("---After Sorting---")
+printlist(studentList)
